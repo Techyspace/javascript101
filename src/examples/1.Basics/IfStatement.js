71b5a41d366a6f559fb76f0a7d1d77e4744b1a1a
Sainty-dev <82646950+Sainty-dev@users.noreply.github.com>
2022-03-04 19:45:13 +0200

    Learn If else.

---
+++ src/examples/1.Basics/IfStatement.js
@@ -0,0 +1,23 @@
+var isValid = false;
+
+//simple if else
+if(isValid){
+  console.log('Yes!. is valid.')
+}
+else{
+    console.log('Yes!. is not valid.')
+}
+
+// else if  (provide extra condition)
+
+var age = 70;
+
+if(age < 18){
+    console.log('Not allowed, young')
+}
+else if (age >= 18 && age <= 50){
+    console.log('allowed!!, adult')
+}
+else{
+    console.log('Not allowed!!, too adult')
+}
