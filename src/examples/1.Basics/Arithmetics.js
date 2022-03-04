eff96f66fd482786e09250fb7e2b8f621a1f4603
Sainty-dev <82646950+Sainty-dev@users.noreply.github.com>
2022-03-04 14:22:13 +0200

    Learn Arithmetics

---
+++ src/examples/1.Basics/Arithmetics.js
@@ -0,0 +1,28 @@
+/*
+ +  addition
+ -  subtraction
+ /  division
+ %  remainder
+ *  multiplication
+
+*/
+
+var sum = 3 + 3;
+
+var product = 3 * 3;
+
+var remainder = 5 % 2;
+
+var division = 20 / 5;
+
+var subtraction = 10 - 4;
+
+
+// print
+
+console.log(sum);
+console.log(product);
+console.log(remainder);
+console.log(division);
+console.log(subtraction);
+
