8300591a448b56ee0535317be6c740fbc67bedc5
Sainty-dev <82646950+Sainty-dev@users.noreply.github.com>
2022-03-04 21:38:44 +0200

    Learn Operators.

---
+++ src/examples/1.Basics/ComparisonLogicalOperators.js
@@ -0,0 +1,27 @@
+/*comparison Operators */
+
+console.log(5 == 5); //true
+console.log(5 <= 5); //true
+console.log(5 >= 5); //true
+console.log(5 < 5); //false
+console.log(5 > 5); //false
+console.log(5 != 5); //false
+
+/*Logical  Operators */
+
+
+console.log(10 == 10 && 11 == 11); //true
+console.log(10 ==11 && 11 == 11) //false
+
+
+console.log(10 ==10 || 11 == 12) //true
+console.log(10 ==13 || 11 == 12) //false
+
+
+console.log(true && true) // ==> true
+console.log(true && false) // ==>false
+console.log(false && false) // ==> false
+
+console.log(true || true) // ==> true
+console.log(true || false) // ==>true
+console.log(false || false) // ==> false
